c542f68545b94bdfca9380853f89a41957b51933
TranLeMinh1234 <tlMinh10300@gmail.com>
2021-10-01 21:28:41 +0700

    minh

--- chat_client/src/App.vue
+++ chat_client/src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div></div>
+      <div class="chat-box">
+        <div class="msg-box">
+          <div v-for="(item, index) in listMes" 
+          :key="index" 
+          :class="['msg', item.account == 0? 'self':'friend']" 
+          >
+            {{item.msg}}
+          </div>
+        </div>
+        <div class="do-chat">
+          <input type="text" class="in-text" v-model="mesSend.msg"  @keypress.enter="send()">
+          <button class="btn-send" @click="send()" >Send</button>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  methods: {
+    // hàm gửi tin nhắn
+    send()
+    {
+      let me = this;
+      me.listMes.push(me.mesSend)
+    },
+
+    receive()
+    {
+      let me = this;
+      let mesSocket = '';// gán tin nhắn vào biến này
+      //xử lí nhận tin nhắn từ socket
+
+
+      // dẩy tin nhắn
+      me.mesReceive.msg = mesSocket;
+      me.listMes.push(me.mesReceive);
+    }
+    
+  },
+  data() {
+    return {
+      // đối tượng phòng
+      room:
+      {
+        id: '',//trường mặc định đừng xóa
+        // muốn thêm trường j vào cứ thêm vd (vietanh: true, long: false)
+      },
+
+      mesReceive: 
+      {
+        msg: '',
+        account:1,
+      },
+      mesSend: 
+      {
+        msg: '',
+        account:0,
+      },
+      // data fake
+      listMes: [
+        {
+          account: 0,
+          msg: 'helo minh',
+        },
+        {
+          account: 1,
+          msg: "Helo Tho"
+        }
+      ],
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('./assets/css/main.css');
 </style>
